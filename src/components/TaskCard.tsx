1bbc13465aa826b80f8661340f71b15be5065487
caleb-freitas <calebfreitas@tutanota.com>
2022-11-03 21:17:15 -0300

    feat: added styles to TaskCard component

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -7,9 +7,16 @@ type TaskTitle = {
 export const TaskCard = ({ title }: TaskTitle) => {
   return (
     <form className="mb-3 flex w-[736px] items-center justify-between rounded-lg border-[1px] border-gray-4000 bg-gray-5000 p-4">
-      <div className="flex items-baseline gap-3">
-        <input className="rounded-full bg-gray-4000" type="checkbox" />
-        <p className="text-gray-1000">{title}</p>
+      <div className="flex items-center gap-3">
+        <input
+          id="checked-checkbox"
+          type="checkbox"
+          value=""
+          className="peer h-4 w-4 rounded-full bg-gray-4000 ring-2 ring-blue-6000 ring-offset-0 checked:bg-purple-6000 checked:ring-purple-6000"
+        />
+        <p className="text-gray-1000 peer-checked:text-gray-3000 peer-checked:line-through">
+          {title}
+        </p>
       </div>
       <button
         className="rounded p-1.5 text-gray-300 transition hover:bg-gray-4000 hover:text-red-3000"
